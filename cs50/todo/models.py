62f0ac7b2321ad083ce6ded657f20d0ec6f4b404
Faizan <faizan@Faizans-MacBook-Air.local>
2022-06-19 12:41:02 +0100

    test models added

--- cs50/todo/models.py
+++ cs50/todo/models.py
@@ -1,3 +1,15 @@
+import email
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+# a toy User table example to test django models
+class Users(models.Model):
+    name = models.CharField(max_length=128)   # create 'name' field
+    email = models.CharField(max_length=128)  # creat 'email' field
+
+# an Items table to represent task items 
+class Items(models.Model):
+    name=models.CharField(max_length=128) 
+    date=models.DateField(auto_now_add=True) # automatically add date only when the object is created
+    status=models.SmallIntegerField(default=0) # 0 = notstarted (default valueq§), 1 = started, 2 = finished

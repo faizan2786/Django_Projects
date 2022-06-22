cec32650fb07e888cf4d289a03607c23d2dde5da
Faizan Patel <faizan2786@hotmail.com>
2022-06-22 16:08:27 +0100

    Customised admin model page  options added

--- cs50/flights/admin.py
+++ cs50/flights/admin.py
@@ -1,9 +1,20 @@
+from ast import Pass
 from django.contrib import admin
-
-# Register your models here.
 
 from .models import Flight, Airport, Passenger
 
-admin.site.register(Flight)
+# create customise display for flight model (in admin interface)
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ('flight', )  # using horizontal filter view on 'flight' (many-to-many) field
+                                    # (to have side by side list of selected and unselected flights for each passenger)
+
+
+# Register your models here.
+
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Airport)
-admin.site.register(Passenger)
+admin.site.register(Passenger, PassengerAdmin)  
